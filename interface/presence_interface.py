4f552d6bb402b618f584657ec4752e9382e8ae44
WilliamFraga21 <william.fraga@fatec.sp.gov.br>
2024-06-10 14:21:53 -0300

    Melhorias

--- interface/presence_interface.py
+++ interface/presence_interface.py
@@ -12,21 +12,26 @@ import csv
 
 class PresenceSystem:
     def __init__(self):
+        # Inicialização da interface gráfica
         self.root = tk.Tk()
         self.root.title("Sistema de Presença")
         
+        # Etiquetas para exibir o número de alunos presentes e ausentes
         self.students_present_label = tk.Label(self.root, text="Alunos Presentes: 0")
         self.students_present_label.pack()
         
         self.students_absent_label = tk.Label(self.root, text="Alunos Ausentes: 0")
         self.students_absent_label.pack()
         
+        # Caixa de texto para exibir a lista de alunos presentes
         self.students_present_text = tk.Text(self.root, height=10, width=50)
         self.students_present_text.pack()
         
+        # Botão para iniciar/parar o reconhecimento facial
         self.toggle_button = tk.Button(self.root, text="Iniciar Reconhecimento", command=self.toggle_recognition)
         self.toggle_button.pack()
         
+        # Variáveis de controle
         self.recognition_running = False
         self.students_present = set()
         self.all_known_students = set()
@@ -36,19 +41,19 @@ class PresenceSystem:
             self.known_face_encodings, self.known_face_names = pickle.load(f)
         
         # Obter todos os alunos conhecidos
-        for name in self.known_face_names:
-            self.all_known_students.add(name)
+        self.all_known_students.update(self.known_face_names)
         
+        # Configuração para fechar a janela corretamente
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
         
         # Criar o arquivo de registro de presença se não existir
         self.create_log_file()
         
     def toggle_recognition(self):
+        # Método para alternar entre iniciar e parar o reconhecimento facial
         if self.recognition_running:
             self.recognition_running = False
             self.toggle_button.config(text="Iniciar Reconhecimento")
-            # Redefinir os dados quando o reconhecimento é interrompido
             self.reset_data()
         else:
             self.recognition_running = True
@@ -56,67 +61,49 @@ class PresenceSystem:
             threading.Thread(target=self.start_recognition).start()
             
     def start_recognition(self):
+        # Método para iniciar o reconhecimento facial em uma thread separada
         video_capture = cv2.VideoCapture(0)
-        
         while self.recognition_running:
-            # Limpar a lista de alunos presentes
             self.students_present.clear()
-            
             ret, frame = video_capture.read()
+            if not ret:
+                continue
             
-            # Converter o frame de BGR (OpenCV) para RGB (face_recognition)
             rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            
-            # Encontrar todas as faces e encodings no frame atual
             face_locations = face_recognition.face_locations(rgb_frame)
             face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
             
             for face_encoding in face_encodings:
-                # Comparar o rosto com os rostos conhecidos
                 matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
                 name = "Desconhecido"
                 
-                # Calcular a menor distância para encontrar o melhor ajuste
                 face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
                 best_match_index = np.argmin(face_distances)
                 if matches[best_match_index]:
                     name = self.known_face_names[best_match_index]
-                    
-                    # Verificar se o nome está no formato esperado
                     if name.count('_') == 3:
                         nome, ra, curso, periodo = name.split('_')
                     else:
                         nome, ra, curso, periodo = ("Desconhecido", "Desconhecido", "Desconhecido", "Desconhecido")
                     
-                    # Obter data e hora atuais
                     now = datetime.now()
                     data = now.strftime("%Y-%m-%d")
                     hora = now.strftime("%H:%M:%S")
                     
-                    # Inserir presença no banco de dados
                     self.insert_presence(ra, nome, curso, periodo, data, hora)
-                    
-                    # Adicionar entrada no arquivo de registro
                     self.add_to_log(ra, nome, curso, periodo, data, hora)
-                    
-                    # Adicionar o nome do aluno à lista de alunos presentes
                     self.students_present.add(name)
                 else:
                     print("Aluno não conhecido")
             
-            # Atualizar a interface com o número de alunos presentes e ausentes
             self.update_presence_labels()
-            
-            # Atualizar o campo de texto com a lista de alunos presentes
             self.update_present_list()
-            
-            # Aguardar 3 segundos antes da próxima verificação
             time.sleep(3)
         
-        # Liberar a captura de vídeo
         video_capture.release()
 
     def insert_presence(self, ra, nome, curso, periodo, data, hora):
+        # Método para inserir dados de presença no banco de dados SQLite
         conn = sqlite3.connect('database/presenca.db')
         cursor = conn.cursor()
         cursor.execute('''
@@ -128,12 +115,14 @@ class PresenceSystem:
         print(f"Dados do aluno {nome} inseridos com sucesso no banco de dados.")
     
     def create_log_file(self):
+        # Método para criar o arquivo de log de presença se ele não existir
         with open('presenca_log.csv', 'w', newline='') as csvfile:
             fieldnames = ['RA', 'Nome', 'Curso', 'Periodo', 'Data', 'Hora']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
     
     def add_to_log(self, ra, nome, curso, periodo, data, hora):
+        # Método para adicionar dados de presença ao arquivo de log de presença
         with open('presenca_log.csv', 'a', newline='') as csvfile:
             fieldnames = ['RA', 'Nome', 'Curso', 'Periodo', 'Data', 'Hora']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -141,6 +130,7 @@ class PresenceSystem:
         print(f"Dados do aluno {nome} adicionados ao log de presença.")
     
     def update_presence_labels(self):
+        # Método para atualizar as etiquetas na interface gráfica com o número de alunos presentes e ausentes
         students_present_count = len(self.students_present)
         students_absent_count = len(self.all_known_students) - students_present_count
         
@@ -148,23 +138,23 @@ class PresenceSystem:
         self.students_absent_label.config(text=f"Alunos Ausentes: {students_absent_count}")
     
     def update_present_list(self):
-        # Limpar o campo de texto antes de atualizar a lista
+        # Método para atualizar o texto na caixa de texto com a lista de alunos presentes
         self.students_present_text.delete(1.0, END)
-        
-        # Adicionar cada aluno presente ao campo de texto
         for student in self.students_present:
             self.students_present_text.insert(tk.END, f"{student}\n")
     
     def reset_data(self):
-        # Limpar os conjuntos de alunos presentes e ausentes
+        # Método para limpar os dados de alunos presentes
         self.students_present.clear()
     
     def on_close(self):
+        # Método chamado ao fechar a janela da aplicação
         if messagebox.askokcancel("Fechar", "Deseja fechar o sistema?"):
             self.recognition_running = False
-            self.reset_data()  # Redefinir os dados ao fechar
+            self.reset_data()
             self.root.destroy()
 
 if __name__ == "__main__":
+    # Ponto de entrada do programa
     app = PresenceSystem()
     app.root.mainloop()
--- src/train_model.py
+++ src/train_model.py
@@ -5,28 +5,40 @@ import pickle
 # Diretório onde as imagens dos alunos estão armazenadas
 ALUNOS_DIR = 'alunos/'
 
-# Função para carregar as imagens e criar os embeddings
-def train_model():
-    known_face_encodings = []
-    known_face_names = []
+def load_images(directory):
+    """
+    Carrega imagens de um diretório e retorna as codificações faciais e os nomes das pessoas nelas contidas.
+    """
+    face_encodings = []
+    face_names = []
 
-    for root, dirs, files in os.walk(ALUNOS_DIR):
+    for root, dirs, files in os.walk(directory):
         for filename in files:
             if filename.endswith(('jpg', 'jpeg', 'png')):
-                # Carregar a imagem
                 img_path = os.path.join(root, filename)
-                image = face_recognition.load_image_file(img_path)
-                
-                # Obter o encoding da face
-                face_encodings = face_recognition.face_encodings(image)
-                
-                if face_encodings:
-                    known_face_encodings.append(face_encodings[0])
-                    known_face_names.append(os.path.basename(root))
+                try:
+                    image = face_recognition.load_image_file(img_path)
+                    encoding = face_recognition.face_encodings(image)[0]
+                    face_encodings.append(encoding)
+                    face_names.append(os.path.basename(root))
+                except Exception as e:
+                    print(f"Erro ao processar a imagem {img_path}: {e}")
 
-    # Salvar os dados em um arquivo
-    with open('src/trained_faces.pkl', 'wb') as f:
-        pickle.dump((known_face_encodings, known_face_names), f)
+    return face_encodings, face_names
+
+def save_model(face_encodings, face_names, filename='src/trained_faces.pkl'):
+    """
+    Salva as codificações faciais e os nomes em um arquivo pickle.
+    """
+    with open(filename, 'wb') as f:
+        pickle.dump((face_encodings, face_names), f)
+
+def train_model():
+    """
+    Função principal para treinar o modelo.
+    """
+    face_encodings, face_names = load_images(ALUNOS_DIR)
+    save_model(face_encodings, face_names)
 
 # Treinar o modelo
 train_model()
